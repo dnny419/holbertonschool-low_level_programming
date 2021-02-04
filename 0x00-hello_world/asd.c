ff3f34a0d6910404f55610aef648ee045fd211a0
dnny419 <dnny419@gmail.com>
2021-02-04 21:48:56 +0000

    Adding initial files

---
+++ 0x00-hello_world/asd.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+  for (a = 0; a < b; a++)
+    {
+      printf("%d\n", a);
+    }
+
+  return (0);
+}
