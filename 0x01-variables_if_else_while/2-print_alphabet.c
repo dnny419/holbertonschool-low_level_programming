091942b282d4927da7a34e29a5bad170e2117969
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-05 01:00:11 +0100

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,10 @@
  */
 int main(void)
 {
-  char a;
-
-  for (a = 'a'; a <= 'z' ; a++)
-    putchar(a);
-  putchar('\n');
-
-  return (0);
+	char a;
+	
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
 }
