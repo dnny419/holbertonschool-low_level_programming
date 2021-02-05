abf921545940637cd7e37aebc72d34e738a46e98
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-05 03:54:16 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - Program that prints all possible combinations sdjkfdiu3438743rhfjdkfsdj and22
+ * main - Program that prints all possible combinations
  * of single-digit numbers
  *
  * Return: Always 0 (Success)
