583c4340fe51340734c2099ab66cc00f13acb4ff
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-05 02:31:52 +0100

    Create 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers from 0 to 9
+ * Description: printf
+ * Return: void
+ */
+
+int main(void)
+{
+	int a, b = 10;
+	
+	for (a = 0; a < b; a++)
+	{	
+		printf("%d\n", a);
+	}
+	
+return (0);
+}
+
