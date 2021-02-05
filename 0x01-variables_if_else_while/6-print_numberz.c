00c9266ac5f504a154568118acb664b384af3e2a
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-05 02:36:43 +0100

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program that prints numbers (0 - 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		putchar (num);
+	}
+	putchar(10);
+return (0);
+}
