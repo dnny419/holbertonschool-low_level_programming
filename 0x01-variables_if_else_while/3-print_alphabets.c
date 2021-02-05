226f0cfeaf08ccb06d9052860739a2b9afc0e9e5
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-05 01:47:42 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,19 @@
  * main - ALPHABET
  * Description: use 3 putchar
  * Return: void
- */
+*/
 int main(void)
 {
-  char ch, hc;
+	char ch, hc;
 
-  for (ch = 'a' ; ch <= 'z' ; ch++)
-    {
-      putchar(ch);
-    }
-  for (hc = 'A' ; hc <= 'Z' ; hc++)
-    {
-      putchar(hc);
-    }
-  putchar('\n');
-  return (0);
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (hc = 'A' ; hc <= 'Z' ; hc++)
+	{
+		putchar(hc);
+	}
+	putchar('\n');
+	return (0);
 }
