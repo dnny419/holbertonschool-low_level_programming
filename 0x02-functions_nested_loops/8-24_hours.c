7102b75c82e838ed086331c10c96521fb0b8a5af
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-09 05:37:31 +0100

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,12 +13,11 @@ int jack_bauer(void)
 	{
 		c = a / 10 + '0';
 		d = a % 10 + '0';
-			
+		
 		for (b = 0; b < 60; b++)
 		{
 			e = b / 10 + '0';
 			f = b % 10 + '0';
-			
 			_putchar(c);
 			_putchar(d);
 			_putchar(58);
