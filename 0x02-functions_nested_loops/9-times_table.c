703a612b3199a9a01c75724d58ea80f688124dda
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-09 17:21:36 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 /**
- * times_table - Program that prints the 9 times table
+ * times_table - multiplication table
+ * main - table
  * Description: times 9 table
  * Return: void
- */
+*/
 void times_table(void)
 {
 	int i, j, res;
-	
+
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
@@ -15,23 +16,23 @@ void times_table(void)
 			res = i * j;
 			if (j == 0)
 			{
-				putchar(res + '0');
+				_putchar(res + '0');
 			}
 			else if (res <= 9)
 			{
-				putchar(44);
-				putchar(32);
-				putchar(32);
-				putchar(res + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(32);
+				_putchar(res + '0');
 			}
 			else if (res >= 9)
 			{
-				putchar(44);
-				putchar(32);
-				putchar(res / 10 + '0');
-				putchar(res % 10 + '0');
+				_putchar(44);
+				_putchar(32);
+				_putchar(res / 10 + '0');
+				_putchar(res % 10 + '0');
 			}
 		}
-		putchar(10);
+		_putchar(10);
 	}
 }
