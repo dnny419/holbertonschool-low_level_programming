de1747645ef429be8fc9b45ea84fcf3b85f3cc4c
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-09 17:29:30 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * add - sum two numbers
+ * @a: first variable
+ * @b: second variable
+ * Return: sum btwn a and b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
