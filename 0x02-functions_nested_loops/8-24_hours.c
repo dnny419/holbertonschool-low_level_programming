cd623caf69f04343c2d889d2a8e802416cae7d67
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-09 05:08:05 +0100

    Create 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * jack_bauer - Program that prints hours and minutes
+ *
+ * No return
+ */
+
+int jack_bauer(void)
+{
+	int a, b, c, d, e, f;
+
+	for (a = 0; a < 24; a++)
+	{
+		c = a / 10 + '0';
+		d = a % 10 + '0';
+			
+		for (b = 0; b < 60; b++)
+		{
+			e = b / 10 + '0';
+			f = b % 10 + '0';
+			
+			_putchar(c);
+			_putchar(d);
+			_putchar(58);
+			_putchar(e);
+			_putchar(f);
+			_putchar(10);
+		}
+	}
+}
