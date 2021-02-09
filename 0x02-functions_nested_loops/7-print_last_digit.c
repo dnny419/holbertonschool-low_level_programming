c817dc4351ae0b74df4db6be43ea45ab3d48855d
dnny419 <77732644+dnny419@users.noreply.github.com>
2021-02-09 01:29:04 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * print_last_digit - print last digit
+ * @a: value to know
+ * Return: last digit
+ */
+int print_last_digit(int n)
+{
+	int a;
+	
+	if (n < 0)
+	{
+		a = a * -1;
+	}
+	else
+	{
+		a = n % 10;
+	}
+	
+	_putchar (a + '0');
+	return (a);
+}
