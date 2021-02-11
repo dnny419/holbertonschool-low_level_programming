7e09e50ab3b607bff85ef4c94a2b83f799ff7824
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-11 18:45:23 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - multiply two integers
+ * @a: variable to determinate
+ * @b: variable to determinate
+ * Return: Always 0. success
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
