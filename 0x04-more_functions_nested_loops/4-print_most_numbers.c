0f7ad3dc2fb4fda754843acb306374647fee3523
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-11 19:40:14 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,15 +5,16 @@
 * Description: 0 to 9 followed by new line
 * Return: void
 */
-int print_most_numbers(void)
+int main(void)
 {
 	int a;
 
 	for (a = 48; a <= 57; a++)
 	{
-		if(a != 50 && a != 52)
+		if((a != 50) && (a != 52))
 		{
-			putchar(a);
+			_putchar(a);
 		}
 	}
+	_putchar(10);
 }
