d917b1090b45fa319f96ed241d7fb707c4737898
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-11 21:42:31 +0100

    Update 6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,12 +12,12 @@ void print_line(int n)
 	{
 		for (a = 1; a <= n; a++)
 		{
-			putchar(95);
+			_putchar(95);
 		}
-		putchar(10);
+		_putchar(10);
 	}
 	else
 	{
-		putchar(10);
+		_putchar(10);
 	}
 }
