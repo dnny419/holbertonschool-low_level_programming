f55c726cf9ebac52471c36211676bdad54f42f49
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-11 19:30:26 +0100

    Create 4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+* print_numbers - Short description, single line
+* main - print numbers
+* Description: 0 to 9 followed by new line
+* Return: void
+*/
+int main(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		if((a != 50) && (a != 52))
+		{
+			putchar(a);
+		}
+	}
+	putchar(10);
+}
