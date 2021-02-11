ecf7ecd271f4cf99ec45871b9ab1036ac30a9d5b
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-11 18:55:49 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,12 +1,12 @@
 #include "holberton.h"
 /**
- * _isupper - determinate if c is upper
- * @c: variable to determinate
+ * _isupper - determinate if the character is upper
+ * @c: character to determinate
  * Return: Always 0. success
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 98)
+	if (c >= 65 && c <= 90)
 		return (1);
 	return (0);
 }
