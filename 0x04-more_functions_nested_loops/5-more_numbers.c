ebeeccf5c005d3a3755042f58378ca835e8b8ab9
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-11 20:57:11 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+* more_numbers - Short description, single line
+* main - print more numbers
+* Description: 0 to 14 x10 times followed by new line
+* Return: void
+*/
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b >= 10)
+			{
+				putchar(b / 10 + '0');
+			}
+
+			putchar(b % 10 + '0');
+		}
+		putchar(10);
+	}
+}
