942f4ae0ac94efc2ca0b5721948c1d79171c4bbf
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-12 03:39:54 +0100

    Create 10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * print_diagonal - make a fancy diagonal!
+ * @size: number of lines
+ *
+ */
+void print_triangle(int size)
+{
+	int n = 20, a, b, c, d, e;
+
+	if (n > 0)
+	{
+		a = n;
+		b = n - 1;
+		
+		for (c = 1; c <= a; c++)
+		{
+			for (d = 1; d <= b; d++)
+			{
+				putchar(32);
+			}
+			b--;
+			for (e = 1; e <= c; e++)
+			{
+				putchar(35);
+			}
+			putchar(10);
+		}
+	}
+	else
+	{
+		putchar(10);
+	}
+}
