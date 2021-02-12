d1d125519a9a32495ad3370e90642d42cb2e0222
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-12 02:14:45 +0100

    Update 9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,24 +7,24 @@
 int main(void)
 {
 	int a, b;
-	
+
 	for (a = 1; a <= 100; a++)
 	{
-		if(a % 15 == 0)
+		if (a % 15 == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		if(a % 3 == 0)
+		else if (a % 3 == 0)
 		{
 			printf("Fizz ");
 		}
-		else if(a % 5 == 0)
+		else if (a % 5 == 0)
 		{
 			printf("Buzz ");
 		}
 		else
 		{
-			printf("%d ", a);
+			printf ("%d ", a);
 		}
 	}
 	return (0);
