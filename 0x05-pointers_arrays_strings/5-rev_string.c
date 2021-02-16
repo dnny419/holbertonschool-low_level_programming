1e31ac3963295b266ddd32e75ba0dac4267a48be
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 04:45:40 +0100

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+/**
+* rev_string - reverse a string
+* @s: string
+*
+* Return: void
+*/
+void rev_string(char *s)
+{
+    char aux[100];
+	int i, j;
+
+	j = 0;
+    for (i = 0; s[i] != '\0'; i++)
+    {
+		aux[i] = s[i];
+    }
+    aux[i] = 0;
+    i--;
+	for (; i >= 0; i--)
+    {
+		s[j] = aux[i];
+		j++;
+    }
+}
