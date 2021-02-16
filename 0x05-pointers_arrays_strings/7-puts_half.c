03ea2cec7b125551be34a2c286caf7597f7ba6bb
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 20:56:03 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,3 +1,4 @@
+#include "holberton.h"
 /**
 * puts_half - print half of a string
 * @str: string
@@ -6,18 +7,18 @@
 */
 puts_half(char *str)
 {
-    int a, k;
-    int length;
-    int length2;
+	int a, k;
+	int length;
+	int length2;
 
-    length = _strlen(s) / 2;
-    length2 = _strlen(s);
+	length = _strlen(s) / 2;
+	length2 = _strlen(s);
 
-    for(a = length, k = 0; a <= length2; a++, k++)
-    {
-	    putchar(s[a]);
-    }
-    putchar('\n');
+	for (a = length, k = 0; a <= length2; a++, k++)
+	{
+		putchar(s[a]);
+	}
+	putchar('\n');
 }
 
 int _strlen(char *s)
