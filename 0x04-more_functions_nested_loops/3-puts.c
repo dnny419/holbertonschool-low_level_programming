dc8a9eda65a7d164ff7b73baee808279dcdfa8b6
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 01:56:19 +0100

    Create 3-puts.c

---
+++ 0x04-more_functions_nested_loops/3-puts.c
@@ -0,0 +1,16 @@
+/**
+* print_rev - print a string, followed by a new line
+* @s: string
+*
+* Return: void
+*/
+void print_rev(char *s)
+{
+    int a;
+
+    for (a = 0; s[a] != '\0'; a++)
+	{
+		_putchar(s[a]);
+    }
+    _putchar('\n');
+}
