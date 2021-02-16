7555a722648dc37f450b0e9d772db0c1f32316bb
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 05:13:51 +0100

    Update 1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,12 +1,12 @@
 /**
- * swap_int - swapping integers!
- * @a: a first integer
- * @b: b second integer
- *
- */
-void swap_int(int* a, int* b)
+* swap_int - swapping integers!
+* @a: a first integer
+* @b: b second integer
+*
+*/
+void swap_int(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
