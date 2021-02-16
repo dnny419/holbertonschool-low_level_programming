2c4dde86e3b11d9c6860019e76c2539056a85052
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 18:28:04 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,19 +7,20 @@
 */
 void rev_string(char *s)
 {
-	char aux[100];
+	char aux[1000];
 	int i, j;
 
 	j = 0;
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 	{
 		aux[i] = s[i];
 	}
 	aux[i] = 0;
 	i--;
-	for (; i >= 0; i--)
+	for (; i >= 0 ; i--)
 	{
 		s[j] = aux[i];
 		j++;
 	}
+
 }
