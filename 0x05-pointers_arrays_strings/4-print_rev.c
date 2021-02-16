1f0fe42ca931043e34318c723b3f8103e7ef4c98
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 05:25:32 +0100

    Update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,16 +6,16 @@
 */
 void print_rev(char *s)
 {
-    int a;
+	int a;
 
-    for (a = 0; s[a] != '\0'; a++)
-    {
+	for (a = 0; s[a] != '\0'; a++)
+	{
 
-    }
-    a--;
-    while (a >= 0)
-    {
-	putchar(s[a--]);
-    }
-    putchar(10);
+	}
+	a--;
+	while (a >= 0)
+	{
+		putchar(s[a--]);
+	}
+	putchar(10);
 }
