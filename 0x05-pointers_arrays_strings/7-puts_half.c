2bb061265fa1fed18d200dce70ab758f2dee9650
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 20:52:20 +0100

    Create 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+/**
+* puts_half - print half of a string
+* @str: string
+*
+* Return: void
+*/
+puts_half(char *str)
+{
+    int a, k;
+    int length;
+    int length2;
+
+    length = _strlen(s) / 2;
+    length2 = _strlen(s);
+
+    for(a = length, k = 0; a <= length2; a++, k++)
+    {
+		putchar(s[a]);
+    }
+    putchar('\n');
+}
+
+int _strlen(char *s)
+{
+	int a;
+
+	while (s[a] != '\0')
+	{
+		a++;
+	}
+	return (a);
+}
