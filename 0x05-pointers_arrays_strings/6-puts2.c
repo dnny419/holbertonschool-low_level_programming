8e3395264038453d5d2437e4ff30df5d321f20f3
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-16 05:06:41 +0100

    Create 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+/**
+* puts2 - print every other character of a string
+* @s: string
+*
+* Return: void
+*/
+void puts2(char *s)
+{
+    int a;
+
+    for (a = 0; s[a] != '\0'; a++)
+    {
+	    if(a % 2 == 0)
+	    {
+			  putchar(s[a]);
+		  }
+    }
+    putchar('\n');
+}
