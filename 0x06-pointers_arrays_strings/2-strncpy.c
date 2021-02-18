239d99eeaf03df2c1c9056ed75a96d5559ed01b7
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-18 22:10:32 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
-* _strncat - concadenate two strings with n bytes
+* _strncat - copy a string
 * @dest: ptr dest
 * @src: ptr src
 * @n: n bytes
@@ -10,14 +10,16 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
-  
-	for (i = 0; src[i] != '\0'; i++, ++length)
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		if (i < n)
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];	
 	}
-	dest[length] = 0;
+	
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+    }
+	  
 	return (dest);
 }
