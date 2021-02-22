00221f308dfd2b733d3f7c47cf67e05cf6b28036
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-23 00:17:25 +0100

    Create 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ * _strspn - Get the length of a prefix substring
+ * @s: string
+ * @accept: accepted characters
+ *
+ * Return: count
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j, count = 0;
+
+	if(s == NULL || accept == NULL)
+	{
+		exit(0);
+	}
+	else
+	{
+		for (i = 0; *(s + i) != 32 && *(s + i) != '\0'; i++)
+		{
+			for (j = 0 ; *(accept + j) != '\0'; j++)
+			{
+				if(*(s + i) == *(accept + j))
+				{
+					count++;
+					break;
+				}
+			}
+		}
+		return (count);
+	}
+}
