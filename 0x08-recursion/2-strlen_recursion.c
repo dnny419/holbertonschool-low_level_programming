adb23d9a62b4bb3453cb15e784eed8a7ba2b8dcf
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-24 21:23:43 +0100

    file iniziale

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strlen_recursion - count a string with recursion
+ * @s: string to print
+ *
+ * Return: void
+*/
+int _strlen_recursion(char *s)
+{
+	if (*s == NULL)
+	{
+		return(0);
+	}
+	else
+	{
+		return(1 + _strlen_recursion(s + 1));
+	}
+}
