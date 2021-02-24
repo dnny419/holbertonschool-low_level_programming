c392eb1ba70312952cc387b2f817f2d3612a2a80
not Dnny <77732644+dnny419@users.noreply.github.com>
2021-02-24 21:41:54 +0100

    File iniziale

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * factorial - return factorial of
+ * @n: number
+ *
+ * Return: void
+*/
+int factorial(int n)
+{
+	if (n <  0)
+	{
+		return(-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return n * factorial (n - 1);
+	}
+}
