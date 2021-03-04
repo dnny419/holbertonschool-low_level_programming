17d0d777d4e3a46bd2cb34dfd78f5326154a3543
dnny419 <dnny419@gmail.com>
2021-03-04 21:01:15 +0000

    Fix!

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -9,30 +9,29 @@
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	size_t i, j, k, sum;
+	size_t lenght = 0, lenght2 = 0, i, j = 0;
 	char *a;
 
-	if (s1 == NULL)
-		i = 0;
+	if (s1 == NULL || s1 == '\0')
+		lenght = 0;
 	else
 		for (i = 0; s1[i] != '\0'; i++)
-			;
-	if (s2 == NULL)
-		j = 0;
+			lenght++;
+	if (s2 == NULL || s2 == '\0')
+		lenght2 = 0;
 	else
-		for (j = 0; s2[j] != '\0'; j++)
-			;
-	/*---------- do not mind me, just spacing here! ----------*/
-	if (j > n)
-		j = n;
-	a = malloc(i + j + 1 * sizeof(char));
-	if (a == NULL) /*failing to allocate memory */
+		for (i = 0; s2[i] != '\0'; i++)
+			lenght2++;
+	/*---------- do not mind me, just spacing! ----------*/
+	if (n >= lenght2)
+		n = lenght2;
+	a = malloc(sizeof(char) * (lenght + n + 1));
+	if (a == NULL)
 		return (NULL);
-	for (k = 0; k < i; k++)
-		a[k] = s1[k];
-	for (k = 0; k < j; k++)
-		a[i + k] = s2[k];
-	a[i + j] = '\0';
-
+	for (i = 0; i < lenght; i++)
+		a[i] = s1[i];
+	for (i = lenght; i < lenght + n; i++, j++)
+		a[i] = s2[j];
+	a[lenght + n] = '\0';
 	return (a);
 }
