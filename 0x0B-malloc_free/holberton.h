1bdc7e08c1a5468062f44d7bef52118430d02aed
dnny419 <dnny419@gmail.com>
2021-03-04 02:41:53 +0000

    Fixing header file

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -7,5 +7,8 @@ char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
+char *_strcat(char *dest, char *src);
+int _strlen(char *s);
+char *argstostr(int ac, char **av);
 
 #endif /* _HOLBERTON_H_ */
