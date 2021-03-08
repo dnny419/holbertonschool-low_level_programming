2d506e805afb5af5fc90c225d35860f1712dc3ff
dnny419 <dnny419@gmail.com>
2021-03-08 20:16:46 +0000

    Fix!

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -1,7 +1,7 @@
-#ifndef _3_FUNCTION_LIKE_MACRO_H_
-#define _3_FUNCTION_LIKE_MACRO_H_
+#ifndef _SUM_H_
+#define _SUM_H_
 
 #define SUM(x, y) \
 	((x) + (y))
 
-#endif /* _3_FUNCTION_LIKE_MACRO_H_ */
+#endif /* _SUM_H_ */
