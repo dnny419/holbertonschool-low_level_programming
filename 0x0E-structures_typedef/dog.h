3d8f7e6392a49bfe660317c43597ccf70eabb85f
dnny419 <dnny419@gmail.com>
2021-03-08 21:38:25 +0000

    First commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - Info about doggies
+ * elements of dog struc:
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* _DOG_H_ */
