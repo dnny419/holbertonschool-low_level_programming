6eab5f47e1c3cb550c0bad2e5a4d2b4ec47f8906
dnny419 <dnny419@gmail.com>
2021-03-09 02:35:30 +0000

    Yo! Free that dog!

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * free_dog - function that frees dogs by its address:
+ * @d: ptr address of the dog info
+ *
+ * Return: Nothing.
+ */
+void free_dog(dog_t *d)
+{
+	/** if the address is different from 0 we can then initialize its elements*/
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
