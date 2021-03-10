5b80fd51532a4df8f22f8797ac1c4a5377db4529
dnny419 <dnny419@gmail.com>
2021-03-10 22:53:37 +0000

    First commit

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that searches for an integer
+ * @array: array itself
+ * @size: size of the array
+ * @cmp: function used to compare values
+ *
+ * Return: Integer.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	/* if size is less than or equal to than 0 or array is equal to null */
+	if (size <= 0 || array == NULL)
+	{
+		return (-1);
+	}
+	/* else if cmp is different from 0 aka it does exist */
+	else if (cmp)
+	{
+		for (int i = 0; i < size; i++)
+		{
+			if (cmp(array[i]))
+			{
+				return (i);
+			}
+		}
+	}
+	/* return -1 in any other weird scenario, DO NOT ask, it just works! */
+	return (-1);
+}
