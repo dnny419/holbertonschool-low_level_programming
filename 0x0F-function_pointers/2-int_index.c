63093ae5f0d972af728f420ea8425ab215c5d5f9
dnny419 <dnny419@gmail.com>
2021-03-10 23:02:18 +0000

    Fix for task 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,19 +10,16 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	/* if size is less than or equal to than 0 or array is equal to null */
-	if (size <= 0 || array == NULL)
+	if (size <= 0 || array == NULL || cmp == NULL)
 	{
 		return (-1);
 	}
 	/* if cmp is different from 0 aka it does exist */
-	if (cmp)
+	for (int i = 0; i < size; i++)
 	{
-		for (int i = 0; i < size; i++)
+		if (cmp(array[i]))
 		{
-			if (cmp(array[i]))
-			{
-				return (i);
-			}
+			return (i);
 		}
 	}
 	/* return -1 in any other weird scenario, DO NOT ask, it just works! */
