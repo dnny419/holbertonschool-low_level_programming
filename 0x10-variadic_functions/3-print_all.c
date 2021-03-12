fa365ddfdf75a72ef285da74c6f81676687ed9a1
dnny419 <dnny419@gmail.com>
2021-03-12 00:35:35 +0000

    First commit

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,88 @@
+#include "variadic_functions.h"
+/**
+ * print_c - print char
+ * @v: Value to be printed
+ * Return: void
+ */
+void print_c(va_list v)
+{
+	printf("%c", va_arg(v, int));
+}
+
+/**
+ * print_i - print integer
+ * @v: Value to be printed
+ * Return: void
+ */
+void print_i(va_list v)
+{
+	printf("%d", va_arg(v, int));
+}
+
+/**
+ * print_s - print string
+ * @v: Value to be printed
+ * Return: void
+ */
+void print_s(va_list v)
+{
+	char *str = va_arg(v, char *);
+
+	if (str == NULL)
+	{
+		str = "(nil)";
+	}
+	else
+	{
+		printf("%s", str);
+	}
+}
+
+/**
+ * print_f - print float
+ * @v: Value to be printed
+ * Return: void
+ */
+void print_f(va_list v)
+{
+	printf("%f", va_arg(v, double));
+}
+
+/**
+ * print_all - function to print everything
+ * @format: list of types of arguments passed to the function
+ * Return: void
+ */
+void print_all(const char *const format, ...)
+{
+	size_t i, j;
+	op_t ops[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"i", print_i},
+		{"f", print_f},
+		{NULL, NULL}};
+	va_list valist;
+	char *empty = "";
+	char *separator = ", ";
+
+	va_start(valist, format);
+	i = 0;
+	while (format && format[i])
+	{
+		j = 0;
+		while (ops[j].op != NULL)
+		{
+			if (format[i] == ops[j].op[0])
+			{
+				printf("%s", empty);
+				ops[j].f(valist);
+				empty = separator;
+			}
+			j++;
+		}
+		i++;
+	}
+	va_end(valist);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -5,9 +5,28 @@
 #include <stddef.h>
 #include <stdio.h>
 
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+	char *op;
+	void (*f)(va_list);
+} op_t;
+
 int _putchar(char);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+/* prototypes to print everything */
+void print_c(va_list v);
+void print_i(va_list v);
+void print_s(va_list v);
+void print_f(va_list v);
+void print_all(const char *const format, ...);
 
 #endif /* _VARIADIC_FUNCTIONS_H_ */
