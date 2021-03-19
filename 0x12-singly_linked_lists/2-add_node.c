cea0e023735c8d9e3c802927ea4ad940c6cb6938
Carlos Galeano <dnny419@gmail.com>
2021-03-20 05:28:37 +0700

    First commit

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * _strlen - function that gets the length of a string
+ * @str: string
+ * Return: length of the string
+ */
+unsigned int _strlen(char *str)
+{
+	unsigned int i;
+
+	for (i = 0; str[i]; i++)
+		;
+
+	return (i);
+}
+/**
+ * add_node - function that adds a new node at the end of a list_t list
+ * @head: head
+ * @str: string
+ * Return: the number of nodes
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
+
+	new->str = strdup(str);
+	new->len = _strlen(new->str);
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,8 @@
 #ifndef _LISTS_H_
 #define _LISTS_H_
 
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include <stddef.h>
 
@@ -23,5 +25,7 @@ typedef struct list_s
 int _putchar(char);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+unsigned int _strlen(char *str);
 
 #endif /* _LISTS_H_ */
