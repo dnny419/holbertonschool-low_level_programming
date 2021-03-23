eab4313563eb1b9a12e18673e26ae3113fdaeb30
Carlos Galeano <dnny419@gmail.com>
2021-03-23 08:18:14 +0700

    First commit

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a list_t list.
+ * @head: string - (malloc'ed string)
+ *
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *nextElement;
+	int i;
+
+	for (i = 0; head != NULL; i++)
+	{
+		nextElement = (*head)->next;
+		free(*head);
+		*head = nextElement;
+	}
+}
