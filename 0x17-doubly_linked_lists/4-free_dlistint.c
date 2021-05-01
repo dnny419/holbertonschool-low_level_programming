e489c1c8cf16a2f28dea44a45005afc79a7562e2
Carlos Galeano <dnny419@gmail.com>
2021-05-02 06:27:30 +0700

    First commit

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * free_dlistint - function that frees a list.
+ * @head: head
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+
+	for (size_t i = 0; head != NULL; i++)
+	{
+		tmp = head;
+		head = head->next;
+		free(tmp);
+	}
+}
