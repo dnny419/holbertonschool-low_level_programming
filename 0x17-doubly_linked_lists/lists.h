35a6bee25a54885301a09d53fbdf9210c9ebb347
Carlos Galeano <dnny419@gmail.com>
2021-05-02 05:13:30 +0700

    First commit

---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* data structure */
+/**
+ * struct dlistint_s - double linkedlist
+ * @prev: ptr to the previous node
+ * @n: integer
+ * @next: ptr to the next node
+ *
+ */
+
+typedef struct dlistint
+{
+	int n;
+	struct dlistint *prev;
+	struct dlistint *next;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
